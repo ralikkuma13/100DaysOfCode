b75567bb5032e11d51df3b3ab68cd8b5edc49369
Mitsuki Tatebe <tatebe.mitsuki@gmail.com>
2018-05-24 13:45:57 +0900

    Day 2
    Revisited the Dog class I’ve made the previous day, which is just a code representation of a dog which takes a few parameters (like age, name, etc.) and used a while loop to go through an array I’ve created to represent strings of items the dog fetched. Other than that, went through if-else statements, the different loops,
    Also created a new project making use of the scanner class allowing the user to enter an age and printing a String depending on what age was input.

---
+++ lynda chapter3-2/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Enter an age: ");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+
+        if(age >= 0 && age <= 5){
+            System.out.println("Baby");
+        }
+        else if(age >= 6 && age <= 11) {
+            System.out.println("Kid");
+        }
+        else if(age >= 12 && age <= 17){
+            System.out.println("Teen")
+        }
+        else if(age >= 18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Invalid");
+        }
+
+        System.out.println("Thanks for using this program");
+
+    }
+}

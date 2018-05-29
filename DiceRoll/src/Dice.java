3a14f8534f8e0bfd281a3d210036785c50ed760a
Mitsuki Tatebe <tatebe.mitsuki@gmail.com>
2018-05-29 12:12:28 +0900

    Day 4
    Edited the coin class so it flips at initial build unlike previously where it is set to a default final value.

---
+++ DiceRoll/src/Dice.java
@@ -0,0 +1,12 @@
+public class Dice {
+
+    //Create a variable that holds user input to know how many dice to roll
+    //Create a dice that contains the numbers 1-6 in an array
+
+    //Roll method
+    //for loop to go through the array picking a random number bound to 6 so it gives out a random number from 0 to 5
+    //which matches the index for the array the dice values are put in
+
+    
+
+}

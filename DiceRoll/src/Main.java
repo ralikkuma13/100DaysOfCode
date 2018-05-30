d169802f7adb2a4103b516f414def96d89a581a8
ralikkuma13 <33254607+ralikkuma13@users.noreply.github.com>
2018-05-30 11:13:04 +0900

    Day 6
    Rewrote the Main class, using try and catch statements. Catching InputMismatchException and using a boolean value to use for the following if statement to print the error message.

--- DiceRoll/src/Main.java
+++ DiceRoll/src/Main.java
@@ -2,18 +2,33 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
+
 public class Main {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        boolean fail = false;
+        int inputI = 0;
         System.out.println("Enter a number of die/dice to roll: ");
-        int inputI = input.nextInt();
 
-        while(inputI <= 0) {
-            System.out.println("Invalid. Try again: ");
-            inputI = input.nextInt();
+        try
+        {
+             inputI = input.nextInt();
+        }
+        catch(InputMismatchException e)
+        {
+            fail = true;
         }
 
+        if(fail || inputI <= 0){
+            System.out.println("INVALID INPUT");
+        }
+
+
+     /*   while(inputI <= 0 || round((float)inputI) <= 0) {
+            System.out.println("Invalid. Try again: ");
+            inputI = input.nextInt();
+        }*/
 /*
             try {
                 inputI = input.nextInt();
